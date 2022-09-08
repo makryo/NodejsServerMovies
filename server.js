7ca6473ed06469e2b692d683eec9f4a401d21624
makryo <38221593+makryo@users.noreply.github.com>
2022-09-08 14:12:30 -0600

    port and host fixed

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(
 
 app.use(rutas);
 
-app.listen(3000, () => {
+const host = '0.0.0.0';
+const port = process.env.port || 3000;
+
+app.listen(port, host, () => {
     console.log("Esta corriendo");
 });
